cb2f9f5f793132c3a87881007fc6d2305905ec95
RoyiDamari <royi66.damari@gmail.com>
2025-02-26 20:35:51 +0200

    Change the methods in subsystems file to statics

--- facade_pattern/smart_home_facade.py
+++ facade_pattern/smart_home_facade.py
@@ -3,21 +3,19 @@ from subsystems import Lights, TV, AirConditioner, PopcornMaker
 
 class SmartHomeFacade:
 
-    def __init__(self, lights = Lights(), tv = TV(), ac = AirConditioner(), popcorn_maker = PopcornMaker()):
+    def __init__(self, lights=Lights(), tv=TV(), ac=AirConditioner(), popcorn_maker=PopcornMaker()):
         self.lights = lights
         self.tv = tv
         self.ac = ac
         self.popcorn_maker = popcorn_maker
 
     def say_good_morning(self):
-
         print("\n=== Good Morning Routine ===")
         self.lights.turn_on()
         self.tv.turn_on(channel="Music Channel")
         self.ac.set_temperature(22)
 
     def say_watch_movie(self):
-
         print("\n=== Watch Movie Routine ===")
         self.popcorn_maker.turn_on()
         self.popcorn_maker.make_popcorn()
@@ -26,13 +24,8 @@ class SmartHomeFacade:
         self.tv.turn_on(channel="Netflix")
         self.ac.set_temperature(18)
 
-
     def say_good_night(self):
-
         print("\n=== Good Night Routine ===")
         self.lights.turn_off()
         self.tv.turn_off()
         self.ac.set_temperature(28)
-
-
-
--- facade_pattern/subsystems.py
+++ facade_pattern/subsystems.py
@@ -1,33 +1,41 @@
 class Lights:
-    def turn_on(self):
+    @staticmethod
+    def turn_on():
         print("Lights: ON")
 
-    def turn_off(self):
+    @staticmethod
+    def turn_off():
         print("Lights: OFF")
 
 
 class TV:
-    def turn_on(self, channel=None):
+    @staticmethod
+    def turn_on(channel=None):
         if channel:
             print(f"TV: ON (channel = {channel})")
         else:
             print("TV: ON")
 
-    def turn_off(self):
+    @staticmethod
+    def turn_off():
         print("TV: OFF")
 
 
 class AirConditioner:
-    def set_temperature(self, temperature):
+    @staticmethod
+    def set_temperature(temperature):
         print(f"Air Conditioner: Set to {temperature}°C")
 
 
 class PopcornMaker:
-    def turn_on(self):
+    @staticmethod
+    def turn_on():
         print("Popcorn Maker: ON")
 
-    def make_popcorn(self):
+    @staticmethod
+    def make_popcorn():
         print("Popcorn Maker: Popcorn is ready!")
 
-    def turn_off(self):
+    @staticmethod
+    def turn_off():
         print("Popcorn Maker: OFF")
